5881e74d6027ddd0a3a7719721ff47b43e13af61
taheramintaherelzoghby <tataamen678@gmail.com>
2024-07-25 18:26:17 +0300

    edits

--- lib/core/routing/app_routes.dart
+++ lib/core/routing/app_routes.dart
@@ -3,24 +3,28 @@ import 'package:flutter/material.dart';
 import 'routes.dart';
 
 class AppRouter {
-  Route? generateRoute(RouteSettings settings) {
+  static Route? generateRoute(RouteSettings settings) {
     final arguments = settings.arguments;
     switch (settings.name) {
       case Routes.onBoardingScreen:
         return MaterialPageRoute(
-          builder: (context) => Scaffold(),
+          builder: (context) => const Scaffold(
+            body: Center(
+              child: Text("Taher"),
+            ),
+          ),
         );
       case Routes.loginScreen:
         return MaterialPageRoute(
-          builder: (context) => Scaffold(),
+          builder: (context) => const Scaffold(),
         );
       case Routes.signUpScreen:
         return MaterialPageRoute(
-          builder: (context) => Scaffold(),
+          builder: (context) => const Scaffold(),
         );
       case Routes.homeScreen:
         return MaterialPageRoute(
-          builder: (context) => Scaffold(),
+          builder: (context) => const Scaffold(),
         );
       default:
         return MaterialPageRoute(
